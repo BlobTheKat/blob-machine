8a0ba030df6faa5235d872d05af0082e9274eab2
BlobKat <blob.kat@hotmail.com>
2023-01-17 16:12:40 +0000

    example mod

---
+++ examplemods/trollcell.js
@@ -0,0 +1,17 @@
+const { BACKWARD, FORWARD, LEFT, RIGHT, UP, DOWN, sound, BEAT, BREAK, cell, DIRECTIONAL, NORMAL } = CELLMODDING
+
+//trollge
+cell({
+  name: 'Troll mover',
+  tx: 0, ty: 1,
+  push(dir, f){
+    const behind = this.get(dir + 2)
+    if(behind)behind.rot(BACKWARD)
+    sound(BEAT)
+    return -Infinity
+  },
+  update: TICK,
+  tick(){
+    this.rot(random() < .5 ? RIGHT : LEFT)
+  }
+})
